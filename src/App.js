cab4d44845f8f17f92855d34758b055856651470
vitaliihair <vitobarbersochi@gmail.com>
2024-01-18 20:53:38 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState (false);
+  const [error, setError] = useState('');
+
+  useEffect(()=> {
+    setLoading(true);
+    setError('');
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .finally(() => setLoading(false))
+    .then(response => response.json())
+    .then(users => setUsers(users))
+    .catch(error => setError(error.toString()))
+  },[])
+  return (
+    <div className="App">
+     {
+     loading ? <h1>Loading...</h1> :
+     error ? <p>Error: {error}</p> :
+     users.map(user => (<>
+     <h5>{user.name}</h5>
+     <p>{user.email}</p>
+     </>))}
+    </div>
+  );
+}
+
+export default App;
